29ece76fe8e46623206ee3f62f6baae78164097f
tahar issad <issadtahar@icloud.com>
2023-02-09 14:29:27 +0100

    etablish_conn etablie une connexion avec un serveur mysql

--- php/session.php
+++ php/session.php
@@ -3,6 +3,18 @@
         private $username;
         private $passwoerd;
 
-
+        public function __construct($username, $passwoerd){
+            $this->username = $username;
+            $this->password = $passwoerd;
+        }
+        
+        public function etablish_conn($servername, $dbusername, $dbpassword, $dbname){
+            $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+            // Vérification de la connexion
+            if (!$conn) {
+                die("Connexion échouée : " . mysqli_connect_error());
+            }
+            return $conn;
+        }
     }
 ?>
